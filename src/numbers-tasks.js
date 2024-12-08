b6e8b51356655f0a47c966d6eb1fa44c5f2ac765
Ольга Синцева <olga.sinzeva@gmail.com>
2024-12-08 19:07:43 +0300

    feat: add functions getLinearEquationRoot, getLastDigit, parseNumberFromString, getParallelepipedDiagonal,roundToPowerOfTen

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -89,8 +89,9 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  const x = (0 - b) / a;
+  return x;
 }
 
 /**
@@ -127,8 +128,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const lastDigit = value % 10;
+  return lastDigit;
 }
 
 /**
@@ -142,8 +144,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -159,8 +161,9 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  const diagonalSqrt = a ** 2 + b ** 2 + c ** 2;
+  return Math.sqrt(diagonalSqrt);
 }
 
 /**
@@ -180,8 +183,9 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const level = 10 ** pow;
+  return Math.round(num / level) * level;
 }
 
 /**
